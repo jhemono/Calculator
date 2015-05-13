6f8ce0c3ca532d3fd4f36cc63a8a5b06bb74238e
Julien Hémono <jhemono@me.com>
2015-05-13 16:45:21 +0200

    Too much to tell

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,108 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Julien Hémono on 13/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private enum Op: Printable
+    {
+        case Operand(Double)
+        case UnaryOperation(String, Double -> Double)
+        case BinaryOperation(String, (Double, Double) -> Double)
+        
+        var description: String {
+            get {
+                switch self {
+                case .Operand(let operand):
+                    return operand.description
+                case .UnaryOperation(let symbol, _):
+                    return symbol
+                case .BinaryOperation(let symbol, _):
+                    return symbol
+                }
+            }
+        }
+    }
+    
+    private var knownOps = [String:Op]()
+    
+    private var opStack = [Op]()
+    
+    init()
+    {
+        func addBinaryOp (op: String, fun: (Double, Double) -> Double)
+        {
+            knownOps[op] = Op.BinaryOperation(op, fun)
+        }
+        func addUnaryOp (op: String, fun: Double -> Double)
+        {
+            knownOps[op] = Op.UnaryOperation(op, fun)
+        }
+        
+        addBinaryOp("×", *)
+        addBinaryOp("÷") { $1 / $0 }
+        addBinaryOp("+", +)
+        addBinaryOp("−") { $1 - $0 }
+        addUnaryOp("√", sqrt)
+        addUnaryOp("sin", sin)
+        addUnaryOp("cos", cos)
+        knownOps["π"] = Op.Operand(M_PI)
+    }
+    
+    func pushOperand(operand: Double) -> Double?
+    {
+        opStack.append(Op.Operand(operand))
+        return evaluate()
+    }
+    
+    func performOperation (symbol: String) -> Double?
+    {
+        if let operation = knownOps[symbol]
+        {
+            opStack.append(operation)        }
+        return evaluate()
+    }
+    
+    private func evaluate (ops: [Op]) -> (result: Double?, remainingOps: [Op])
+    {
+        if !ops.isEmpty {
+            var remainingOps = ops
+            let op = remainingOps.removeLast()
+            switch op {
+            case .Operand(let operand):
+                return (operand, remainingOps)
+            case .UnaryOperation(_, let operation):
+                let operandEvalutation = evaluate(remainingOps)
+                if let operand = operandEvalutation.result {
+                    return (operation(operand), operandEvalutation.remainingOps)
+                }
+            case .BinaryOperation(_, let operation):
+                let op1Evalutation = evaluate(remainingOps)
+                if let operand1 = op1Evalutation.result {
+                    let op2Evalutation = evaluate(op1Evalutation.remainingOps)
+                    if let operand2 = op2Evalutation.result {
+                        return (operation(operand1, operand2), op2Evalutation.remainingOps)
+                    }
+                }
+            }
+        }
+        return (nil, ops)
+    }
+    
+    func evaluate () -> Double?
+    {
+        let (result, remainder) = evaluate(opStack)
+        println("\(opStack) = \(result) with \(remainder) left over")
+        return result
+    }
+    
+    func clear () {
+        opStack = []
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var history: UILabel!
     
+    let brain = CalculatorBrain()
+    
     func log(line: String) {
         if history.text! == "Empty History" {
             history.text = line
@@ -25,10 +27,10 @@ class ViewController: UIViewController {
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
-        if digit == "." && display.text!.rangeOfString(digit) != nil && userIsInTheMiddleOfTypingANumber {
-            return
-        }
         if userIsInTheMiddleOfTypingANumber {
+            if digit == "." && display.text!.rangeOfString(digit) != nil {
+                return
+            }
             display.text = display.text! + digit
         }
         else {
@@ -36,85 +38,49 @@ class ViewController: UIViewController {
             userIsInTheMiddleOfTypingANumber = true
         }
     }
-    var operandStack = Array<Double>()
     
     @IBAction func enter() {
-        if userIsInTheMiddleOfTypingANumber {
+        if let value = displayValue,
+               result = brain.pushOperand(value) {
             log(display.text!)
+            userIsInTheMiddleOfTypingANumber = false
+            displayValue = result
+        } else {
+            displayValue = nil
         }
-        userIsInTheMiddleOfTypingANumber = false
-        operandStack.append(displayValue)
-        println("\(operandStack)")
     }
     
-    var displayValue: Double {
+    var displayValue: Double? {
         get {
-            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+            return NSNumberFormatter().numberFromString(display.text!)?.doubleValue
         }
         set {
-            display.text = "\(newValue)"
-            userIsInTheMiddleOfTypingANumber = false
+            if let newValue = newValue {
+                display.text = "\(newValue)"
+            } else {
+                display.text = "???"
+            }
         }
     }
-    
-    typealias action = () -> ()
     
     @IBAction func operand(sender: UIButton) {
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
         let operation = sender.currentTitle!
-        var fun: action? = nil
-        switch operation {
-            case "×":
-                fun = performOperation({$0 * $1})
-            case "÷":
-                fun = performOperation({$1 / $0})
-            case "+":
-                fun = performOperation({$0 + $1})
-            case "−":
-                fun = performOperation({$1 - $0})
-            case "√":
-                fun = performOperationUnary({sqrt($0)})
-            case "sin":
-                fun = performOperationUnary({sin($0)})
-            case "cos":
-                fun = performOperationUnary({cos($0)})
-            case "π":
-                fun = insertConstant(M_PI)
-            default:
-                fun = nil
-        }
-        if let fun = fun {
-            fun()
+        if let result = brain.performOperation(operation) {
+            displayValue = result
             log(operation)
-            enter()
-        }
-    }
-    
-    func performOperation(operation: (Double, Double) -> Double) -> action? {
-        if (operandStack.count >= 2) {
-            return { self.displayValue = operation(self.operandStack.removeLast(), self.operandStack.removeLast()) }
+            log("=")
+
         } else {
-            return nil
+            displayValue = nil
         }
-    }
-    
-    func performOperationUnary(operation: Double -> Double) -> action? {
-        if (operandStack.count >= 1) {
-            return { self.displayValue = operation(self.operandStack.removeLast()) }
-        } else {
-            return nil
-        }
-    }
-    
-    func insertConstant(constant: Double) -> action? {
-        return { self.displayValue = constant }
     }
     
     @IBAction func clear() {
+        brain.clear()
         userIsInTheMiddleOfTypingANumber = false
-        operandStack = []
         display.text = "0"
         history.text = "Empty History"
     }
