47cb97fc5f2333706f7553b2935fb14ac4d779f6
Julien Hémono <jhemono@me.com>
2015-05-16 00:22:23 +0200

    Graph View

---
+++ Calculator/GraphView.swift
@@ -0,0 +1,21 @@
+//
+//  GraphView.swift
+//  Calculator
+//
+//  Created by Julien Hémono on 16/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class GraphView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
