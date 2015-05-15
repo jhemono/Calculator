e83f5d42f9977a515303739c0ee884d4f422879d
Julien Hémono <jhemono@me.com>
2015-05-15 23:12:28 +0200

    GraphViewController and segue from button

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Julien Hémono on 15/05/15.
+//  Copyright (c) 2015 Julien Hémono. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+    
+
+}
