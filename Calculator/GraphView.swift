6b7ade521839fc7baefbba9b823c4defa268a92f
Julien Hémono <jhemono@me.com>
2015-05-16 09:50:13 +0200

    Graph view draws its axes and it's IBDesignable

--- Calculator/GraphView.swift
+++ Calculator/GraphView.swift
@@ -8,14 +8,22 @@
 
 import UIKit
 
+@IBDesignable
 class GraphView: UIView {
+    
+    let axesDrawer = AxesDrawer()
+    
+    var originOffset = CGPointZero
+    
+    var origin: CGPoint {
+        let centre = convertPoint(center, fromView: superview)
+        return CGPoint(x: centre.x + originOffset.x, y: centre.y + originOffset.y)
+    }
 
-    /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func drawRect(rect: CGRect) {
-        // Drawing code
+        axesDrawer.drawAxesInRect(rect, origin: origin, pointsPerUnit: 1)
     }
-    */
 
 }
